3f795f6f3e59c5354507566a4daaec6d35905aac
MD Faisal <sheikhfaisal689@gmail.com>
2020-03-06 10:35:37 +0530

    added a file

--- temp.rb
+++ temp.rb
@@ -1,27 +1,4 @@
-def  first_non_repeating_letter(s) 
-    oarr,ouniq =[],[]
-    arr = s.chars
-    for i in arr
-        oarr.append(i.ord)
-    end
-    unique = arr.uniq
-    for i in unique
-        ouniq.append(i.ord)
-    end
-    dif = ouniq.difference(oarr)
-
-
-    print dif
-    # print ouniq
-
-    # arr = (str.chars).ord
-
-    #  print arr
-    # unique = arr.uniq
-    # dif = unique.difference(arr)
-    # # puts dif
-
-    # (arr - (arr.difference(arr.uniq)))
-  end
-
-  first_non_repeating_letter('stress')  
+asd
+asd
+ad
+asd
